a00b5ee8a94d902f56e1a4e7351b03f54a9cb71d
Nurgeldy Samekeyev <n.samekeyev@gmail.com>
2024-10-04 16:56:24 +0500

    tester

---
+++ script.sh
@@ -0,0 +1,9 @@
+# URL с которого загружаются данные
+url="https://stepik.org/api/users/972461821"
+
+# Используйте curl для получения данных и jq для извлечения join_date
+user_ID=$(curl -s "$url" | jq -r '.users[0].id')
+
+# Вывод join_date
+echo "User ID: $user_ID"
+
